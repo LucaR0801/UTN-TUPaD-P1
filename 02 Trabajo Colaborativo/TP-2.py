62b354060a061ccbfd65c27ae57d6fd9c2c4eda1
Luca <lucaruff2001@gmail.com>
2025-04-03 13:30:49 -0300

    se agrego tp2

---
+++ 02 Trabajo Colaborativo/TP-2.py
@@ -0,0 +1,13 @@
+#Actividad 1
+print("Hola Mundo")
+
+#Actividad 2
+nombre = input("Ingrese su nombre: ")
+print(f"Hola {nombre}!")
+
+#Actividad 3
+nombre = input(f"Ingrese su nombre: ")
+apellido = input(f"Ingrese su apellido {nombre}: ")
+edad = int(input(f"Ingrese su edad {nombre} {apellido}: "))
+residencia = input(f"¿Cual es su lugar de residencia {nombre} {apellido}?: ")
+print(f"Soy {nombre} {apellido}, tengo {edad} años y vivo en {residencia}.")
